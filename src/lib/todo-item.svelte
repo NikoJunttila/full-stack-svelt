7cd4f56dbe7eb69698e587aed1a07bf81f5af23f
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2023-04-14 19:38:47 +0300

    test

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+	<form action="" method="">
+		<input type="hidden" name="done" value="" />
+		<button aria-label="mark done/not">Done/not</button>
+	</form>
+
+	<form action="" method="">
+		<input type="text" name="save" value="" />
+		<button aria-label="save">save</button>
+	</form>
+
+	<form action="" method="">
+		<button aria-label="delete">delete</button>
+	</form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Todo from "../lib/todo-item.svelte"
+let newTodo : string = ""
+const title : string = "todo"
+</script>
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1>{title}</h1>
+<form action="" method="">
+    <input  type="text" name="todo" aria-label="Add a todo" placeholder="+ tap to add a todo">
+</form>
+<Todo />
