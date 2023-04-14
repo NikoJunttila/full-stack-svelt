3f1de483e5e6c1518dab638e2efc99b7c2438efc
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2023-04-14 20:32:13 +0300

    css

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,15 +1,65 @@
+<style>
+    div{
+        display: grid;
+        grid-template-columns:  2rem 1fr 2rem;
+        grid-gap: 0.5rem;
+        align-items: center;
+        margin-left: 0.5rem;
+        margin-right: 0.1rem;
+        padding: 0.5rem;
+        background-color: antiquewhite;
+        border-radius: 10px;
+        filter:drop-shadow(2px 4px 6px rgba(0,0,0,0.2));
+        transition: filter 0.2s;
+    }
+    button{
+        width: 2em;
+        height: 2em;
+        border: none;
+        background-color: transparent;
+        background-position: 50% 50%;
+        background-repeat: no-repeat;
+        cursor: pointer;
+    
+    }
+    input{
+        border-radius: 10px;
+        flex: 1;
+    }
+    .toggle{
+        border: 1px solid rgba(0,0,0,0.2);
+        border-radius: 50%;
+        box-sizing: border-box;
+        background-size: 1em auto;
+        content: "";
+    }
+    .save{
+        opacity: 0;
+        color: green;
+    }
+    input:focus + .save, .save:focus {
+        transition: opacity 0.3s;
+        opacity: 1
+    }
+    .delete:hover,.delete:focus{
+        opacity: 0.5;
+    }
+   
+    
+</style>
+
 <div>
 	<form action="" method="">
 		<input type="hidden" name="done" value="" />
-		<button aria-label="mark done/not">Done/not</button>
+		<button class="toggle" aria-label="mark done/not"></button>
 	</form>
 
 	<form action="" method="">
-		<input type="text" name="save" value="" />
-		<button aria-label="save">save</button>
+		<input type="text" name="text" value="" />
+		<button class="save" aria-label="text">save</button>
 	</form>
 
 	<form action="" method="">
-		<button aria-label="delete">delete</button>
+		<button class="delete" aria-label="delete">delete</button>
 	</form>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,48 @@
 <script lang="ts">
-    import Todo from "../lib/todo-item.svelte"
-let newTodo : string = ""
-const title : string = "todo"
+	import Todo from '../lib/todo-item.svelte';
+	let newTodo: string = '';
+	const title: string = 'todo';
 </script>
+<style>
+h1{
+    font-size: 2rem;
+    text-align: center;
+    padding: 1rem 0;
+}
+section{
+    max-width: 40rem;
+    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: center;  
+    padding-bottom: 10px; 
+}
+section :global(input){
+border: 1px solid transparent;
+background-color: transparent;
+color: white;
+border-radius: 20px;
+}
+section :global(input:focus-visible){
+box-shadow: inset 1px 1px 6px black;
+border: 1px solid red !important;
+outline: none; 
+}
+input{
+    text-align: center;
+    height: 2rem;
+    margin-bottom: 0.5rem;
+}
+
+</style>
+
 <svelte:head>
-    <title>{title}</title>
+	<title>{title}</title>
 </svelte:head>
+<section>
 <h1>{title}</h1>
 <form action="" method="">
-    <input  type="text" name="todo" aria-label="Add a todo" placeholder="+ tap to add a todo">
+	<input type="text" name="todo" aria-label="Add a todo" placeholder="+ tap to add a todo" />
 </form>
 <Todo />
+</section>
